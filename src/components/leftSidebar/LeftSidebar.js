6acb1b93488ff3f6514a412c135492aef73c560e
LongVuong <longvuong1920@gmail.com>
2023-09-29 22:53:12 +0700

    add layout sample

---
+++ src/components/leftSidebar/LeftSidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./LeftSidebar.module.scss";
+
+const LeftSidebar = () => {
+  return <div className={styles.leftSidebar}>LeftSidebar</div>;
+};
+
+export default LeftSidebar;
