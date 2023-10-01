1b5f4d3c79f5ff7708b729d34192b6a125e1f4b6
LongVuong <longvuong1920@gmail.com>
2023-10-01 07:03:15 +0700

    fix header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,34 +2,53 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "./Header.module.scss";
 import { BiSearch } from "react-icons/bi";
+import { AiOutlineUserAdd, AiOutlineLogin } from "react-icons/ai";
 
 const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.navLogo}>
-        <Link href="/" className={styles.logoImage}>
-          <Image
+        <div className={styles.nav}>
+          <Link href="/" className={styles.logoImage}>
+            {/* <Image
             src="/assets/travel-logo.png"
             alt="logo"
             width={28}
             height={28}
-          />
-          <p className={styles.logoName}>Travel Blog</p>
-        </Link>
-
-        <div className={styles.searchBar}>
-          <input
-            type="text"
-            placeholder="Search..."
-            className={styles.search}
-          />
-          <BiSearch className={styles.searchIcon} />
+          /> */}
+            <p className={styles.logoName}>TravelBlog</p>
+          </Link>
+          <div className={styles.navBar}>
+            <Link href="/" className={styles.navItem}>
+              Latest
+            </Link>
+            <Link href="/" className={styles.navItem}>
+              Most Viewed
+            </Link>
+            <Link href="/" className={styles.navItem}>
+              Most Reactions
+            </Link>
+          </div>
         </div>
 
-        <div className={styles.navBar}>
+        <div className={styles.userBar}>
+          <div className={styles.searchBar}>
+            <input
+              type="text"
+              placeholder="Search..."
+              className={styles.search}
+            />
+            <BiSearch className={styles.searchIcon} />
+          </div>
           <div className={styles.auth}>
-            <Link href="/">Sign In</Link>
-            <Link href="/">Sign Up</Link>
+            <Link className={styles.btns} href="/sign-in">
+              <AiOutlineLogin className={styles.icon} />
+              Log In
+            </Link>
+            <Link className={styles.btns} href="/sign-up">
+              <AiOutlineUserAdd className={styles.icon} />
+              Sign Up
+            </Link>
           </div>
         </div>
       </div>
