d3bdcf3d66ff1af3196d1dfcb1cf904d1cac10a5
LongVuong <longvuong1920@gmail.com>
2023-10-13 23:14:54 +0700

    fix footer and layout

--- src/pages/layout/DefaultLayout.js
+++ src/pages/layout/DefaultLayout.js
@@ -1,16 +1,13 @@
 import React from "react";
-<<<<<<< HEAD
-=======
-import AuthProtect from "@/components/authProtect/AuthProtect";
->>>>>>> c2c0e0f044f9419864f7bed0b4a32c42530411d7
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
+import styles from "../../styles/Home.module.css";
 
 const DefaultLayout = ({ children }) => {
   return (
     <>
       <Header />
-      <div className={styles["main"]}> {children}</div>
+      <div className={styles.main}>{children}</div>
       <Footer />
     </>
   );
