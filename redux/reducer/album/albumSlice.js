5f8a64b41a942c23c4e1949e6e175d957337a19f
Minh Quân <mquanv12@gmail.com>
2023-11-18 00:01:58 +0700

    ok

--- redux/reducer/album/albumSlice.js
+++ redux/reducer/album/albumSlice.js
@@ -1,6 +1,7 @@
 import { createApiThunk, createAsyncSlice } from "../../reduxToolkit";
 import { METHODS } from "../../../global";
 import { initialState } from "../../../global/initState";
+import { createAction } from "@reduxjs/toolkit";
 
 export const createAlbum = createApiThunk(METHODS.POST, "/album/createAlbum", {
   "Content-Type": "multipart/form-data",
@@ -8,6 +9,13 @@ export const createAlbum = createApiThunk(METHODS.POST, "/album/createAlbum", {
 const createAlbumSlice = createAsyncSlice(
   "createAlbum",
   createAlbum,
-  initialState
+  initialState,
+  {
+    clean: (state) => {
+      state.data = initialState.data;
+    },
+  }
 );
+
+export const clean = createAction(`createAlbum/clean`);
 export default createAlbumSlice.reducer;
